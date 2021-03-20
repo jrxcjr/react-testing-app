f4a679b3f7f63f75a1f7c1198826a17b88a7834e
jrxcjr <75625584+jrxcjr@users.noreply.github.com>
2021-03-20 15:23:04 -0300

    adding new components

---
+++ src/components/MainTitle.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Container from "react-bootstrap/container";
+import Jumbotron from "react-bootstrap/jumbotron";
+
+class MainTitle extends React.Component {
+ render() {
+  return (
+    <React.Fragment>
+   <div>
+    <Jumbotron fluid>
+     <Container>
+      <h1>Fluid jumbotron</h1>
+      <p>
+       This is a modified jumbotron that occupies the entire horizontal space of
+       its parent.
+      </p>
+     </Container>
+    </Jumbotron>
+   </div>
+   </React.Fragment>
+  );
+ }
+}
+
+export default MainTitle;
